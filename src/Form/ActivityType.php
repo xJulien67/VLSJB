ce4f56f78ddff5bc35879f5efdda703c1e8dc1f3
valentin <valentin.lesech@gmail.com>
2019-04-04 17:48:44 +0200

    modif recap et form

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -6,15 +6,25 @@ use App\Entity\Activity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
 
 class ActivityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
+            ->add('date', DateType::Class,
+                array(
+                    'widget' => 'single_text',
+                )
+            )
             ->add('distance')
-            ->add('duration')
+            ->add('duration', TimeType::Class, [
+                "with_seconds" => true,
+                "hours" => range(0, 5),
+            ])
             ->add('place')
             ->add('partner')
             ->add('averagePace')
